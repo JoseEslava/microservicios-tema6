23f22f10201f10cc7be42d22f478380ac96cdc43
JoseEslava <32904288+JoseEslava@users.noreply.github.com>
2023-04-13 16:19:51 -0600

    Add files via upload

--- example-2/scripts/container-registry.tf
+++ example-2/scripts/container-registry.tf
@@ -1,9 +1,9 @@
 # Creates a container registry on Azure so that you can publish your Docker images.
 
 resource "azurerm_container_registry" "container_registry" {
-  name                = "flixtube"
+  name                = "flixtubeitesm"
   resource_group_name = azurerm_resource_group.flixtube.name
-  location            = "westus"
+  location            = "southcentralus"
   admin_enabled       = true
   sku                 = "Basic"
 }
@@ -17,5 +17,6 @@ output "registry_un" {
 }
 
 output "registry_pw" {
+  sensitive = true
   value = azurerm_container_registry.container_registry.admin_password
 }
