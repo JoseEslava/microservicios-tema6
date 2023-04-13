2589b80a0733f0ed01182e74acd7684c42b950ab
JoseEslava <32904288+JoseEslava@users.noreply.github.com>
2023-04-13 16:19:00 -0600

    Add files via upload

--- example-1/scripts/providers.tf
+++ example-1/scripts/providers.tf
@@ -1,5 +1,16 @@
-# Initialises Terraform providers and sets their version numbers.
+# set the Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.51.0"
+    }
+  }
+}
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-    version = "1.38.0"
+  features {}
+
+  subscription_id = "36adccf5-e991-4856-b5a4-60fcbc354bf6"
 }
